30c436d7a710feaeaa78e2802d27b1bdb44f4165
juanmv94 <juanmv94@hotmail.com>
2020-05-01 23:37:50 +0200

    Travellers Tales GFX viewer: first version

---
+++ util/findareas.js
@@ -0,0 +1,18 @@
+for(let i=1;i<=array32u.length;i++) {
+	var count=0;
+	var j=i;
+    if (((array32u[j+7]>>24)==-128) || ((array32u[j+7]>>24)==-128)) while (true) {
+		if ((array32u[j+5]>>24)==-128) {
+			j+=6;
+			count++;
+		}
+		else if ((array32u[j+7]>>24)==-128) {
+			j+=8
+			count++;
+		}
+		else {
+			if (count>20) console.log(postopsxe(i<<2)+"  -> "+postopsxe(j<<2)+" "+count);
+			break;
+		}
+    }
+}
